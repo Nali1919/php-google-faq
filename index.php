6fb04cb2193bbbc41d3a30874135f4b1aa06c110
Nali1919 <my@email.com>
2022-10-14 16:11:19 +0200

    Creo faq

---
+++ index.php
@@ -0,0 +1,127 @@
+<style>
+.center{
+
+    max-width: 1000px;
+    margin: 50px auto;
+}
+</style>
+
+<?php
+
+$navbar=[
+    [
+      'introduzione',
+      'Norme sulla privacy',
+      'Termini di servizio',
+      'Tecnologie',
+      'Domande Frequneti'
+    ]
+
+];
+
+   foreach($navbar as $linkNav){
+    ?>
+   <div>
+    <a href="#"><?= $linkNav ?></a>   
+   </div>
+
+
+    <?php
+    } 
+
+
+
+$text=[
+
+    [
+        'question'=> 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+     [
+        'question'=> 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+     [
+        'question'=> 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+     [
+        'question'=> 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+     [
+        'question'=> 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+     [
+        'question'=> 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'reply'=> 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi 
+        rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. 
+        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+         Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni 
+         su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato
+          l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
+        è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+
+    ],
+
+];
+
+foreach($text as $faq){
+
+
+?>
+
+<div class="center">
+<h2><?= $faq['question']?></h2>
+<p><?= $faq['reply']?></p>
+
+</div>
+
+<?php
+}
+
+
+
+?>
